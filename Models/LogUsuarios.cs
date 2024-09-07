a8124df23c04bc9623338aa1293069d85b1d29bc
Vinicius Reies <viniciusreies019@gmail.com>
2024-09-07 17:07:15 -0300

    feat: adicionado model para log dos usuarios

---
+++ Models/LogUsuarios.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projeto_GestaoContratos.Models
+{
+    [Table("LogUsuarios")]
+
+    public class LogUsuarios
+    { 
+        [Column("Id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("Detalhes")]
+        [Display(Name = "Detalhes")]
+        public string Detalhes { get; set; }
+
+        [Column("EmailUsuario")]
+        [Display(Name = "Email Usuário")]
+        public string EmailUsuario { get; set; }
+        
+    }
+
+}
