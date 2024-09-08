afada692b7dec145bc00826ea73ade00c6bd21ca
Vinicius Reies <viniciusreies019@gmail.com>
2024-09-07 23:10:21 -0300

    fix: Correção de abertura de rota pela url

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -31,5 +31,6 @@ namespace Projeto_GestaoContratos.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Projeto_GestaoContratos.Data;
 
