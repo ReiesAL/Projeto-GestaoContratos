40a75ae7b24b99e970e5b417e379dbbd3c0f2ea2
Vinicius Reies <viniciusreies019@gmail.com>
2024-09-08 20:11:30 -0300

    feat: criado diretório para mapeamento de dados e juntamente com o arquivo para leitura "ContratosMap"

---
+++ Models/Mapping/ContratosMap.cs
@@ -0,0 +1,20 @@
+﻿using CsvHelper.Configuration;
+
+namespace Projeto_GestaoContratos.Models.Mapping
+{
+    // Classe de mapeamento dos dados do arquivo csv herdando a classe principal
+    public class ContratosMap : ClassMap<Contratos>
+    {
+        // Momento que ocorre o mapeamento de onde vem as colunas para onde quero
+        public ContratosMap() { 
+
+            Map(m => m.Nome).Name("Nome");
+            Map(m => m.Cpf).Name("CPF");
+            Map(m => m.Contrato).Name("Contrato");
+            Map(m => m.Produto).Name("Produto");
+            Map(m => m.Vencimento).Name("Vencimento");
+            Map(m => m.Valor).Name("Valor");
+
+        }
+    }
+}
