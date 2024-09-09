1b3cb2a08dd65f0bc179ac66a5c233f8f0c07dcc
Vinicius Reies <viniciusreies019@gmail.com>
2024-09-09 04:35:38 -0300

    refact: novas migrations em base da nova tabela com adição de campo UsuarioInclusao

---
+++ Migrations/20240909073311_endMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projeto_GestaoContratos.Migrations
+{
+    /// <inheritdoc />
+    public partial class endMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UsuarioEmail",
+                table: "Contratos",
+                newName: "UsuarioInclusao");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UsuarioInclusao",
+                table: "Contratos",
+                newName: "UsuarioEmail");
+        }
+    }
+}
